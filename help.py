1d69017809f16e04ef5bfc8f14c700ec71b622f7
sonicnkt <da.schizzle@gmail.com>
2016-09-02 14:00:02 +0200

    minor code cleanup

--- help.py
+++ help.py
@@ -3,6 +3,7 @@ import wx.html
 import webbrowser
 import markdown2
 
+
 class HelpDialog(wx.Dialog):
     def __init__(self, helpFile, title='Temp'):
         """Constructor"""
@@ -13,13 +14,14 @@ class HelpDialog(wx.Dialog):
         self.SetSize((600, 600))
 
     def InitUI(self):
-
         self.panel = wx.Panel(self, wx.ID_ANY)
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.html = help = wx.html.HtmlWindow(self.panel, -1, style=wx.NO_BORDER)
+
+        # http://wxpython-users.1045709.n5.nabble.com/Open-a-URL-with-the-default-browser-from-an-HtmlWindow-td2326349.html
         # Bind LINK Click Event to own Function
         help.Bind(wx.html.EVT_HTML_LINK_CLICKED, self.OnLinkClicked)
-        # http://wxpython-users.1045709.n5.nabble.com/Open-a-URL-with-the-default-browser-from-an-HtmlWindow-td2326349.html
+
         #import codecs
         #file = codecs.open(self.help, "r", "utf-8")
         try:
