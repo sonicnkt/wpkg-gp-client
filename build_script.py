8e326bf39c04926b99890d871f36ce19ae2f80a8
sonicnkt <da.schizzle@gmail.com>
2016-09-07 13:47:14 +0200

    increased version number

--- build_script.py
+++ build_script.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # WPKG-GP Client BUILD SCRIPT
 
-VERSION = "0.9.6.5"  # max 4 number values separated by a "."
+VERSION = "0.9.6.6"  # max 4 number values separated by a "."
 NAME = "WPKG-GP Client"  # Application Name
 AUTHOR = "Nils Thiele"
 INNOSETUPCMD = r'%PROGRAMFILES(X86)%\Inno Setup 5\iscc.exe'  # InnoSetup with PreProcessor Support!
